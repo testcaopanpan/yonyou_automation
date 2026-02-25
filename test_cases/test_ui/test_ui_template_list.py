c0631798269cd50bd8aeac702c87133c355ac013
caopanpan <957406615@qq.com>
2026-02-25 12:47:22 +0800

    完善打印模板设计器列表的新增配置字段并检查过程以及完善用例本身的前置、后置操作

--- test_cases/test_ui/test_ui_template_list.py
+++ test_cases/test_ui/test_ui_template_list.py
@@ -156,6 +156,22 @@ def test_design_properties_default_value(login):
     这条用例仅作打印模板模板属性默认值检查
     '''
     driver = login
+    # 用例前置：检查当前预用模板是否存在，存在就删掉
+    try:
+        element = driver.find_element(By.XPATH, '//*[text()="复制_模板属性验证"]')
+        if element is not None:
+            more = driver.find_element(By.XPATH, '//*[text()="复制_模板属性验证"]/../..//*[@fieldid="iprint_more-btn"]')
+            ActionChains(driver).move_to_element(more).perform()
+            time.sleep(5)
+            driver.find_element(By.XPATH, '//*[@fieldid="iprint_delete-btn"][text()="删除"]').click()
+            WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@fieldid="iprint_app_model_modal_title"]')))
+            driver.find_element(By.XPATH, '//*[@fieldid="iprint_app_model_modal_footer_ok"]').click()
+            WebDriverWait(driver, 20).until(
+                EC.invisibility_of_element_located((By.XPATH, '//*[text()="复制_模板属性验证"]')))
+    except NoSuchElementException:
+        pass
+
     WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.XPATH, '//*[@class="card_name"][text()="生产订单"]')))
     driver.find_element(By.XPATH, '//*[text()="生产订单"]/..//*[@fieldid="iprint_copy-btn"]').click()
@@ -189,34 +205,44 @@ def test_design_properties_default_value(login):
     element = driver.find_element(By.XPATH,'//*[@fieldid="design|InputText|tenantPageCode|Input"]')
     assert not element.is_enabled()
     #验证模板名称与新增该模板配置的名称一致
+    time.sleep(3)
     element = driver.find_element(By.XPATH,'//*[@fieldid="design|InputText|uititle|Input"]')
     assert element.get_attribute("value") == "复制_模板属性验证"
     #验证横向矩阵默认值为1
+    time.sleep(3)
     element = driver.find_element(By.XPATH,'//*[@fieldid="design|InputNumber|MergePrintPagesX|InputNumber"]')
     assert element.get_attribute("value") == "1"
     #验证总想矩阵默认值为1
+    time.sleep(3)
     element = driver.find_element(By.XPATH, '//*[@fieldid="design|InputNumber|MergePrintPagesY|InputNumber"]')
     assert element.get_attribute("value") == "1"
     #验证循环主体为【暂无】
+    time.sleep(3)
     element = driver.find_element(By.XPATH,'//*[@fieldid="design|InputSelect|mainTable|Select_search_input"]')
     assert element.get_attribute("value") == "暂无"
     #验证多页面打印方式默认值为【按单据】
+    time.sleep(3)
     element = driver.find_element(By.XPATH, '//*[@fieldid="design|InputSelect|pageTemplateCircle|Select_search_input"]')
     assert element.get_attribute("value") == "按模板页面"
     #下边这几个验证项是检查属性按钮开关状态的
     #单据间隔打印默认关闭
+    time.sleep(3)
     element = driver.find_element(By.XPATH,'//*[@fieldid="design|InputBool|intervalPrinting|Switch"]')
     assert 'checked' not in element.get_attribute("class")
     #双面打印默认关闭
+    time.sleep(3)
     element = driver.find_element(By.XPATH, '//*[@fieldid="design|InputBool|duplexPrinting|Switch"]')
     assert 'checked' not in element.get_attribute("class")
     #仅打印有效审批意见默认关闭
+    time.sleep(3)
     element = driver.find_element(By.XPATH, '//*[@fieldid="design|InputBool|noPrintReverse|Switch"]')
     assert 'checked' not in element.get_attribute("class")
     #不打印无效抢占审批默认关闭
+    time.sleep(3)
     element = driver.find_element(By.XPATH, '//*[@fieldid="design|InputBool|excludeGrab|Switch"]')
     assert 'checked' not in element.get_attribute("class")
     #不打印自动跳过审批默认关闭
+    time.sleep(3)
     element = driver.find_element(By.XPATH, '//*[@fieldid="design|InputBool|excludeNoUserCompleteAuto|Switch"]')
     assert 'checked' not in element.get_attribute("class")
     print("打印模板设计器模板属性默认值验证通过")
@@ -238,6 +264,22 @@ def test_kongjian_list_drag_default_value(login):
     该用例用于进行打印模板设计器中列表控件的删除、新增、配置后的默认属性验证
     '''
     driver = login
+    # 用例前置：检查当前预用模板是否存在，存在就删掉
+    try:
+        element = driver.find_element(By.XPATH, '//*[text()="复制_列表属性验证"]')
+        if element is not None:
+            more = driver.find_element(By.XPATH, '//*[text()="复制_列表属性验证"]/../..//*[@fieldid="iprint_more-btn"]')
+            ActionChains(driver).move_to_element(more).perform()
+            time.sleep(5)
+            driver.find_element(By.XPATH, '//*[@fieldid="iprint_delete-btn"][text()="删除"]').click()
+            WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@fieldid="iprint_app_model_modal_title"]')))
+            driver.find_element(By.XPATH, '//*[@fieldid="iprint_app_model_modal_footer_ok"]').click()
+            WebDriverWait(driver, 20).until(
+                EC.invisibility_of_element_located((By.XPATH, '//*[text()="复制_列表属性验证"]')))
+    except NoSuchElementException:
+        pass
+
     WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.XPATH, '//*[@class="card_name"][text()="生产订单"]')))
     driver.find_element(By.XPATH, '//*[text()="生产订单"]/..//*[@fieldid="iprint_copy-btn"]').click()
@@ -303,12 +345,77 @@ def test_kongjian_list_drag_default_value(login):
     #验证列表设置弹窗弹出正常
     WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'//*[@id="inputDesignModalHeader"]')))
     list_settings_element = driver.find_element(By.XPATH,'//*[@id="inputDesignModalHeader"]')
+    time.sleep(10)
     assert list_settings_element is not None
 
     #搜索选择字段
-
+    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="请输入查找内容"]')))
+    element = driver.find_element(By.XPATH,'//*[@placeholder="请输入查找内容"]')
+    #搜索并选择字段【开工日期】
+    element.send_keys("开工日期")
+    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'//*[@id="Order.orderProduct.startDate"]')))
+    driver.find_element(By.XPATH,'//*[@id="Order.orderProduct.startDate"]').click()
+    #删除搜索框内容
+    element = driver.find_element(By.XPATH, '//*[@value="开工日期"][@type="search"]')
+    # 模拟control+a键盘全选
+    element.click()
+    element.send_keys(Keys.CONTROL + 'a')
+    # 模拟键盘的删除delete
+    element.send_keys(Keys.DELETE)
+    time.sleep(3)
+    yuansu1 = driver.find_element(By.XPATH,'//*[@title="Order.orderProduct.startDate"]')
+    assert yuansu1 is not None
+    #搜索并选择字段【完工日期】
+    element.send_keys("完工日期")
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="Order.orderProduct.finishDate"]')))
+    driver.find_element(By.XPATH, '//*[@id="Order.orderProduct.finishDate"]').click()
+    # 删除搜索框内容
+    element = driver.find_element(By.XPATH, '//*[@value="完工日期"][@type="search"]')
+    # 模拟control+a键盘全选
+    element.send_keys(Keys.CONTROL + 'a')
+    # 模拟键盘的删除delete
+    element.send_keys(Keys.DELETE)
+    time.sleep(3)
+    yuansu2 = driver.find_element(By.XPATH, '//*[@title="Order.orderProduct.finishDate"]')
+    assert yuansu2 is not None
+    #搜索并选择字段【生产数量】
+    element.send_keys("生产数量")
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="Order.orderProduct.quantity"]')))
+    driver.find_element(By.XPATH, '//*[@id="Order.orderProduct.quantity"]').click()
+    # 删除搜索框内容
+    element = driver.find_element(By.XPATH, '//*[@value="生产数量"][@type="search"]')
+    # 模拟control+a键盘全选
+    element.send_keys(Keys.CONTROL + 'a')
+    # 模拟键盘的删除delete
+    element.send_keys(Keys.DELETE)
+    time.sleep(3)
+    yuansu3 = driver.find_element(By.XPATH, '//*[@title="Order.orderProduct.quantity"]')
+    assert yuansu3 is not None
     #点击确定保存列表
-
+    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'//*[text()="确定"]')))
+    driver.find_element(By.XPATH,'//*[text()="确定"]').click()
+    time.sleep(3)
+    elements = driver.find_elements(By.XPATH, '//*[@id="inputDesignModalHeader"]')
+    assert len(elements) == 0, "列表设置弹窗未关闭"
     #检查列表配置后在画布上展示正常
-
+    new_list_title = driver.find_element(By.XPATH,'//*[@id="watermarkWordCover"]//*[@title="列表: 生产订单产品"]')
+    assert new_list_title is not None
     #点击并检查列表的各默认属性值是否正常
+
+    # 用例后置：删除本用例新增模板
+    driver.switch_to.window(printlist_handle)
+    try:
+        element = driver.find_element(By.XPATH, '//*[text()="复制_列表属性验证"]')
+        if element is not None:
+            more = driver.find_element(By.XPATH, '//*[text()="复制_列表属性验证"]/../..//*[@fieldid="iprint_more-btn"]')
+            ActionChains(driver).move_to_element(more).perform()
+            time.sleep(5)
+            driver.find_element(By.XPATH, '//*[@fieldid="iprint_delete-btn"][text()="删除"]').click()
+            WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@fieldid="iprint_app_model_modal_title"]')))
+            driver.find_element(By.XPATH, '//*[@fieldid="iprint_app_model_modal_footer_ok"]').click()
+            print("本次用例新增模板删除完成，用例执行成功")
+    except NoSuchElementException:
+        pass
